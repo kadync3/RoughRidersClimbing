5fed1c66493bc023ec319e705f7e9d4089541325
kadync3 <106933582+kadync3@users.noreply.github.com>
2023-03-20 14:31:15 -0400

    switching to localImages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect } from 'react';
 import AOS from 'aos'
-
+import '/images/ClimbingPhoto1.jpg'
+import '/images/climberDoodle.png'
 import ScrollTracker from './ScrollTracker';
 
 import 'aos/dist/aos.css'
@@ -18,13 +19,13 @@ useEffect(()=> {
   AOS.init()
 }, [])
   return (
-    <div className="app">
-      <div ref={climber} className='rappelBox' data-aos='slide-right'  ></div>
+    <div className='app'>
+      {/* <div ref={climber} className='rappelBox' data-aos='slide-right'  ></div> */}
       
-    <div className='initialLoad'>
+    <div className='initialLoad'  >
 
     <div className='navBar'>
-      <img src="https://fvmstatic.s3.amazonaws.com/maps/m/ICON-NPS-00-0006.png"></img>
+      <img src='https://fvmstatic.s3.amazonaws.com/maps/m/ICON-NPS-00-0006.png'></img>
       <div className='navElement'>Home</div>
       <div className='navElement'>Memberships</div>
       <div className='navElement'>Visit Us</div>
@@ -41,6 +42,9 @@ useEffect(()=> {
 
     </div>
     <div className='rappelContainer'>
+    <img ref={climber} src='' className='rappelBox' data-aos='slide-right'
+      ></img>
+
    <div className='boxTwo' style={{  marginLeft: '5%', marginTop: '5%'}}> Bouldering</div>
    <div className='boxTwo' style={{ alignSelf:'flex-end', marginRight: '40%', marginTop: '5%'}} >Rope Climbing</div>
    <div className='boxTwo' style={{  marginLeft: '5%',  marginTop: '3%'}}>Slacklineing</div>
